984d596a9108a250c2e0065999b9dda86a48fe9a
unknown <jschoch@engineyard.com>
2009-04-02 13:29:53 -0400

    added untracked files for sound detection

--- ps3eyeMFC/Resource.h
+++ ps3eyeMFC/Resource.h
@@ -3,10 +3,22 @@
 // Used by ps3eyeMFC.rc
 //
 
-#define IDS_APP_TITLE			103
-
+#define IDS_APP_TITLE					103
+#define IDD_WAVEINFFT_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_EQ_MODE                     1000
+#define IDC_SPECTRUM                    1001
+#define IDC_PEAK                        1002
+#define IDC_PEAK_ALT                    1003
+#define IDC_MICRO_EQ                    1004
+#define IDC_OSCILLOSCOPE                1005
+#define IDC_PIXELGRAM                   1006
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
 // Next default values for new objects
 //
+
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE	101
