0151e99fa875f94c38752ddeb7da84d008cb6d27
unknown <jschoch@engineyard.com>
2009-04-03 12:08:47 -0400

    FPS fixed, small sleep in sound listener to lower CPU

--- ps3eyeMFC/ps3eyeMFC.cpp
+++ ps3eyeMFC/ps3eyeMFC.cpp
@@ -19,8 +19,10 @@
 #include <iostream>
 #include <vector>
 #include <cv.h>
-
 #include <cxcore.h>
+
+#include <windows.h>
+#include <sys/timeb.h>
 
 #include <highgui.h>
 #include "IPS3EyeLib.h"
@@ -62,99 +64,14 @@ using namespace std;
 #define Amplitude(re,im,len) (GetFrequencyIntensity(re,im)/(len))
 #define AmplitudeScaled(re,im,len,scale) ((int)Amplitude(re,im,len)%scale)
 
-BOOL Process(void* lpData, LPWAVEHDR pwh)
-{
-#define FFT_LEN 2048
-	double finleft[FFT_LEN/2],finright[FFT_LEN/2],fout[FFT_LEN],foutimg[FFT_LEN],fdraw[FFT_LEN/2];
-	DWORD nCount = 0;
-	for (DWORD dw = 0; dw < FFT_LEN; dw++)
-	{
-		{
-			//copy audio signal to fft real component for left channel
-			finleft[nCount] = (double)((short*)pwh->lpData)[dw++];
-			//copy audio signal to fft real component for right channel
-			finright[nCount++] = (double)((short*)pwh->lpData)[dw];
-		}
-	}
-	//Perform FFT on left channel
-	/*  don't need left channel for decible measure...
-
-	fft_double(FFT_LEN/2,0,finleft,NULL,fout,foutimg);
-	float re,im,fmax=-99999.9f,fmin=99999.9f;
-	for(int i=1;i < FFT_LEN/4;i++)//Use FFT_LEN/4 since the data is mirrored within the array.
-	{
-		re = (float)fout[i];
-		im = (float)foutimg[i];
-		//get amplitude and scale to 0..256 range
-		//fdraw[i]=AmplitudeScaled(re,im,FFT_LEN/2,256);
-		fdraw[i] = ((int)mag_sqrd(re,im))%256;
-		if (fdraw[i] > fmax)
-		{
-			fmax = (float)fdraw[i];
-		}
-		if (fdraw[i] < fmin)
-		{
-			fmin = (float)fdraw[i];
-		}
-	}
-	//Use this to send the average band amplitude to something
-	int nAvg, nBars=16, nCur = 0;
-	for(int i=1;i < FFT_LEN/4;i++)
-	{
-		nAvg = 0;
-		for (int n=0; n < nBars; n++)
-		{
-			nAvg += (int)fdraw[i];
-		}
-		nAvg /= nBars;
-		//Send data here to something,
-		//nothing to send it to so we print it.
-		TRACE("Average for Bar#%d is %d\n",nCur++,nAvg);
-		i+=nBars-1;
-	}
-	*/
-	
-	
-	// Perform FFT on right channel
-	//fmax=-99999.9f,fmin=99999.9f;
-	fft_double(FFT_LEN/2,0,finright,NULL,fout,foutimg);
-	float re,im,fmax=-99999.9f,fmin=99999.9f;
-	fdraw[0] = fdraw[FFT_LEN/4] = 0;
-	double decibelLevel = 0;
-	for(int i=1;i < FFT_LEN/4;i++)//Use FFT_LEN/4 since the data is mirrored within the array.
-	{
-		re = (float)fout[i];
-		im = (float)foutimg[i];
-		//get Decibels in 0-110 range
-		decibelLevel = Decibels(re,im);
-		fdraw[i] = Decibels(re,im);
-		if (fdraw[i] > fmax)
-		{
-			fmax = (float)fdraw[i];
-		}
-		if (fdraw[i] < fmin)
-		{
-			fmin = (float)fdraw[i];
-		}
-	}
-	// Detect Decibel level above threashold
-	if (decibelLevel > 70)
-	{
-		cout<< "Decible Hit!!!!!!!!!!!!!!!"<<endl;
-		
-	}
-	return TRUE;
-	/*else
-	{
-		return false;
-	}
-	*/
-}
 
 
 
+
+// function prototypes
+BOOL Process(void* lpData, LPWAVEHDR pwh);
 void displayAvailableFormats(void);
-
+DWORD WINAPI ThreadProc(LPVOID lpParameter);
 
 
 int main(int argc, char *argv[])
@@ -204,7 +121,9 @@ int main(int argc, char *argv[])
 	
 	bool recordStarted = false;
 	bool movInit = false;
-
+	bool threadStarted = false;
+	DWORD dwThreadID;
+	HANDLE hThread;
 	// expiriment create buffer to store 300 frames, or 3 seconds
 
 	
@@ -235,41 +154,73 @@ int main(int argc, char *argv[])
 	
 	//int maxX = (CAPTURE_WIDTH*CAPTURE_HEIGHT*COLOR_DEPTH)/8;
 	IplImage *image = cvCreateImage(cvSize(CAPTURE_WIDTH, CAPTURE_HEIGHT), IPL_DEPTH_8U, 3);
+	
+	/* moved to thread
 	DataHolder m_data;
 	Recorder m_rec;
+	*/
+	double		m_timePrevFrameStamp = 0;
+	double		m_timeCurrFrameStamp = 0;
+	double		m_nFps = 0.1;
+	double		m_nFpsAlpha = 0.1;
+	struct timeb timeStamp;
+	char buff[128];
+	
 	while(key != 0x1b)
 	{
 		DWORD dwTimeStart=GetTickCount();
 		//IplImage *image = cvCreateImage(cvSize(CAPTURE_WIDTH, CAPTURE_HEIGHT), IPL_DEPTH_8U, 3);
 		if(pCam->GetFrame((PBYTE)image->imageData, COLOR_DEPTH, false, true))
 		{
-			
-			
-			
-
+			/* new FPS display code below
 			long fTimeDifference=GetTickCount()-dwTimeStart;         
-			char buff[128];
+			
 			int nFramesPerSecond =(int)(1000.f/fTimeDifference);
 			//int nFramesPerSecond=1000/fTimeDifference;
 			sprintf( buff, "%d", nFramesPerSecond );
+			*/
+			ftime( &timeStamp );
+
+			m_timeCurrFrameStamp = (double) timeStamp.time*1000 + timeStamp.millitm;
+			//m_timeCurrFrameStamp = m_pWorker->GetTime( );
+			// update fps
+			if( m_nFps < 0 )
+			{
+				m_nFps = 1000 / ( m_timeCurrFrameStamp - m_timePrevFrameStamp );
+			} else
+			{	
+				m_nFps = ( 1 - m_nFpsAlpha ) * m_nFps + m_nFpsAlpha * 
+							1000 / ( m_timeCurrFrameStamp - m_timePrevFrameStamp );
+			}
+			// set current time stamp as previuos
+			m_timePrevFrameStamp = m_timeCurrFrameStamp;
+			//sprintf( buff, "%d", m_nFps );
+			sprintf(buff, "FPS: %5.1f", m_nFps );
+
 			if (record == true )
 			{
 
-				/* need to create ring buffer not short circuit static one
-				if (damnCount >= bufSize)
-				{
-					key = 32;
-					cout << " -BUFFER FULL- ";
-				}*/
+				
 				if (movInit == true)
 				{
 					//int iSize = sizeof *image;
 					//cout << "SizeOf image is: " << iSize <<endl;
 					
+					/* moved to thread
 					m_rec.Open();
 					m_rec.SetBufferFunction((void*)&m_data,Process);
 					m_rec.Start();
+					*/
+					
 
+					if (threadStarted == false)
+						{
+						// start thread
+						cout<< "Create thread to listen for sound trigger"<<endl;
+						LPVOID pRecord = &record;
+						hThread = CreateThread(0,0,ThreadProc,pRecord,0,&dwThreadID);
+						threadStarted = true;
+						}
 					// Create new memory location for image, need to prealocate this in a ring buffer
 					//PBYTE pBuffer=new BYTE[(CAPTURE_WIDTH*CAPTURE_HEIGHT*COLOR_DEPTH)/8]; 
 					
@@ -316,7 +267,15 @@ int main(int argc, char *argv[])
 					record = false;
 					recordStarted = false;
 					movInit = false;
-					m_rec.Stop();
+					threadStarted = false;
+
+					// kill thread
+
+					CloseHandle(hThread);
+
+
+					// need to send thread stop msg
+					// m_rec.Stop();
 					cout << "Attemting to create video writer"<<endl;
 
 					// create ffmpeg video writer
@@ -441,5 +400,112 @@ void displayAvailableFormats(void)
 	}
 } 
 
+BOOL Process(void* lpData, LPWAVEHDR pwh)
+{
+#define FFT_LEN 2048
+	double finleft[FFT_LEN/2],finright[FFT_LEN/2],fout[FFT_LEN],foutimg[FFT_LEN],fdraw[FFT_LEN/2];
+	DWORD nCount = 0;
+	for (DWORD dw = 0; dw < FFT_LEN; dw++)
+	{
+		{
+			//copy audio signal to fft real component for left channel
+			finleft[nCount] = (double)((short*)pwh->lpData)[dw++];
+			//copy audio signal to fft real component for right channel
+			finright[nCount++] = (double)((short*)pwh->lpData)[dw];
+		}
+	}
+	//Perform FFT on left channel
+	/*  don't need left channel for decible measure...
 
-
+	fft_double(FFT_LEN/2,0,finleft,NULL,fout,foutimg);
+	float re,im,fmax=-99999.9f,fmin=99999.9f;
+	for(int i=1;i < FFT_LEN/4;i++)//Use FFT_LEN/4 since the data is mirrored within the array.
+	{
+		re = (float)fout[i];
+		im = (float)foutimg[i];
+		//get amplitude and scale to 0..256 range
+		//fdraw[i]=AmplitudeScaled(re,im,FFT_LEN/2,256);
+		fdraw[i] = ((int)mag_sqrd(re,im))%256;
+		if (fdraw[i] > fmax)
+		{
+			fmax = (float)fdraw[i];
+		}
+		if (fdraw[i] < fmin)
+		{
+			fmin = (float)fdraw[i];
+		}
+	}
+	//Use this to send the average band amplitude to something
+	int nAvg, nBars=16, nCur = 0;
+	for(int i=1;i < FFT_LEN/4;i++)
+	{
+		nAvg = 0;
+		for (int n=0; n < nBars; n++)
+		{
+			nAvg += (int)fdraw[i];
+		}
+		nAvg /= nBars;
+		//Send data here to something,
+		//nothing to send it to so we print it.
+		TRACE("Average for Bar#%d is %d\n",nCur++,nAvg);
+		i+=nBars-1;
+	}
+	*/
+	
+	
+	// Perform FFT on right channel
+	fft_double(FFT_LEN/2,0,finright,NULL,fout,foutimg);
+	float re,im,fmax=-99999.9f,fmin=99999.9f;
+	fdraw[0] = fdraw[FFT_LEN/4] = 0;
+	double decibelLevel = 0;
+	for(int i=1;i < FFT_LEN/4;i++)//Use FFT_LEN/4 since the data is mirrored within the array.
+	{
+		re = (float)fout[i];
+		im = (float)foutimg[i];
+		//get Decibels in 0-110 range
+		decibelLevel = Decibels(re,im);
+		fdraw[i] = Decibels(re,im);
+		if (fdraw[i] > fmax)
+		{
+			fmax = (float)fdraw[i];
+		}
+		if (fdraw[i] < fmin)
+		{
+			fmin = (float)fdraw[i];
+		}
+	}
+	// Detect Decibel level above threashold
+	if (decibelLevel > 70)
+	{
+		cout<< "Decible Hit!!!!!!!!!!!!!!!"<<endl;
+		
+	}
+	return TRUE;
+	/*else
+	{
+		return false;
+	}
+	*/
+}
+DWORD WINAPI ThreadProc(LPVOID lpParameter) 
+{
+bool running = TRUE;
+DataHolder m_data;
+Recorder m_rec;
+while (running)
+{
+	Sleep(2);
+	if (lpParameter)
+	{
+	m_rec.Open();
+	m_rec.SetBufferFunction((void*)&m_data,Process);
+	m_rec.Start();
+	}
+	else
+	{
+		m_rec.Stop();
+		running = FALSE;
+	}
+}
+  return 0;
+}
